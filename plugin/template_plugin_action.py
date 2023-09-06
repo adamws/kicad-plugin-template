ab6406e38708db4209a57d7d9eb7178cd428e048
adamws <adamws@users.noreply.github.com>
2023-09-06 19:48:16 +0200

    Add basic plugin structure and configuration

---
+++ plugin/template_plugin_action.py
@@ -0,0 +1,26 @@
+import os
+
+import pcbnew
+import wx
+
+from .example_dialog import ExampleDialog
+
+
+class TemplatePluginAction(pcbnew.ActionPlugin):
+    def defaults(self) -> None:
+        self.name = "Template Plugin"
+        self.description = "This is plugin template"
+        self.show_toolbar_button = True
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), "icon.png")
+
+    def Run(self) -> None:
+        pcb_frame = next(
+            x for x in wx.GetTopLevelWindows() if x.GetName() == "PcbFrame"
+        )
+
+        dlg = ExampleDialog(pcb_frame)
+        if dlg.ShowModal() == wx.ID_OK:
+            # this plugin does nothing usefull
+            pass
+
+        dlg.Destroy()
